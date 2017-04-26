b4220c68ffe87500b2a4ab03260a5af597863433
Misha Wagner <mishajw@gmail.com>
2017-04-26 22:23:32 +0100

    Create basic Expression type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+enum Expression<'a, T: 'a> {
+    Value(T),
+    Add(&'a Expression<'a, T>, &'a Expression<'a, T>),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = Expression::Value(1);
+    let b = Expression::Value(2);
+    let c = Expression::Add(&a, &b);
+
+    println!("c: {:?}", c);
 }
