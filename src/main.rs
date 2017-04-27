b8116092edcff8347bf507fcd975867a49b99fda
Misha Wagner <mishajw@gmail.com>
2017-04-27 20:22:24 +0100

    Add multiplication to Expression enum

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate num;
 
 use std::boxed::Box;
-use num::CheckedAdd;
+use num::{CheckedAdd, CheckedMul};
 
 #[derive(Debug)]
 enum Expression<T> {
     Value(T),
     Add(Box<Expression<T>>, Box<Expression<T>>),
+    Multiply(Box<Expression<T>>, Box<Expression<T>>),
 }
 
-impl<T: CheckedAdd + Clone> Expression<T> {
+impl<T: CheckedAdd + CheckedMul + Clone> Expression<T> {
     fn evaluate(&self) -> Option<T> {
         match self {
             &Expression::Value(ref a) =>
@@ -18,7 +19,12 @@ impl<T: CheckedAdd + Clone> Expression<T> {
                 match (a.evaluate(), b.evaluate()) {
                     (Some(a), Some(b)) => a.checked_add(&b),
                     _ => None,
-                }
+                },
+            &Expression::Multiply(ref a, ref b) =>
+                match (a.evaluate(), b.evaluate()) {
+                    (Some(a), Some(b)) => a.checked_mul(&b),
+                    _ => None,
+                },
         }
     }
 }
@@ -26,9 +32,12 @@ impl<T: CheckedAdd + Clone> Expression<T> {
 fn main() {
     let a = Expression::Value(1);
     let b = Expression::Value(2);
-    let c = Expression::Add(Box::new(a), Box::new(b));
+    let c = Expression::Value(3);
+    let add = Expression::Add(Box::new(a), Box::new(b));
+    let multiply = Expression::Multiply(Box::new(add), Box::new(c));
+    let final_expression = multiply;
 
-    println!("c: {:?}", c);
-    println!("c.evaluate(): {:?}", c.evaluate());
+    println!("final_expression: {:?}", final_expression);
+    println!("final_expression.evaluate(): {:?}", final_expression.evaluate());
 }
 
