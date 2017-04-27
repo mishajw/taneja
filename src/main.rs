ddb433fbe7790183b90456881d6dee0b4d567611
Misha Wagner <mishajw@gmail.com>
2017-04-27 20:18:12 +0100

    Implement evaluate function for Expression using num library

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+extern crate num;
+
 use std::boxed::Box;
+use num::CheckedAdd;
 
 #[derive(Debug)]
 enum Expression<T> {
     Value(T),
     Add(Box<Expression<T>>, Box<Expression<T>>),
+}
+
+impl<T: CheckedAdd + Clone> Expression<T> {
+    fn evaluate(&self) -> Option<T> {
+        match self {
+            &Expression::Value(ref a) =>
+                Some(a.clone()),
+            &Expression::Add(ref a, ref b) =>
+                match (a.evaluate(), b.evaluate()) {
+                    (Some(a), Some(b)) => a.checked_add(&b),
+                    _ => None,
+                }
+        }
+    }
 }
 
 fn main() {
@@ -12,4 +29,6 @@ fn main() {
     let c = Expression::Add(Box::new(a), Box::new(b));
 
     println!("c: {:?}", c);
+    println!("c.evaluate(): {:?}", c.evaluate());
 }
+
