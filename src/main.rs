3d6d3c2f00c2e7d98253c09f2136c6faf00facb4
Misha Wagner <mishajw@gmail.com>
2017-04-29 19:40:39 +0100

    Print out information about streaks in evaluation results

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,12 @@ fn run_with_length(length: usize) {
             }
         };
 
+        println!("Starting to make expressions...");
         make_possible_expressions(initial_expressions, callback);
+        println!("Done.");
     }
 
-    println!("Expressions:");
-    for (evaluation, expression) in evaluations {
-        println!("{} = {}", expression, evaluation)
-    }
+    print_streak_information(evaluations)
 }
 
 fn get_initial_expression_list(length: usize) -> Vec<Expression<f64>> {
@@ -51,5 +50,50 @@ fn get_initial_expression_list(length: usize) -> Vec<Expression<f64>> {
         .map(|x| x as f64)
         .map(Expression::Value)
         .collect::<Vec<Expression<f64>>>()
+}
+
+fn print_streak_information(evaluations: BTreeMap<i32, Expression<f64>>) {
+    let mut longest_streak = 0i32;
+    let mut longest_streak_start: Option<&i32> = None;
+    let mut longest_streak_end: Option<&i32> = None;
+    let mut streak_beginning_opt: Option<&i32> = None;
+    let mut previous_opt: Option<&i32> = None;
+
+    for evaluation in evaluations.keys() {
+        match previous_opt {
+            Some(previous) => {
+                let difference = evaluation - previous;
+
+                if difference == 1 {
+                    if let Some(streak_beginning) = streak_beginning_opt {
+                        let streak_length = evaluation - streak_beginning;
+
+                        if longest_streak < streak_length {
+                            longest_streak = streak_length;
+                            longest_streak_start = Some(streak_beginning);
+                            longest_streak_end = Some(previous)
+                        }
+                    } else {
+                        streak_beginning_opt = Some(previous)
+                    }
+                } else if difference == 2 {
+                    println!("Missing {}", previous + 1);
+                    streak_beginning_opt = None
+                } else if difference > 2 {
+                    println!("Missing {} to {}", previous + 1, evaluation - 1);
+                    streak_beginning_opt = None
+                }
+            }
+            None => {}
+        }
+
+        previous_opt = Some(evaluation)
+    }
+
+    match (longest_streak_start, longest_streak_end) {
+        (Some(start), Some(end)) =>
+            println!("Longest streak from {} to {} with length of {}", start, end + 1, longest_streak),
+        _ => {}
+    }
 }
 
